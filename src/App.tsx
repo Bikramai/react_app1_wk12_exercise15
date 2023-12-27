e9c8e8094676c679f6ee6cf4411611c0682a8db7
bikramia <bikram.rhce@gmail.com>
2023-12-27 13:34:30 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchUsers = async() => {
+      try {
+        const response = await axios
+          .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+        setUsers(response.data);
+      }
+      catch(err) {
+        setError((err as AxiosError).message)
+      }
+    }
+
+    fetchUsers();
+    
+    // get -> await promise -> response / err
+      // .then((Response) => setUsers(Response.data))
+      // .catch((err) => setError(err.message));
+  }, []);
+
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+    </>
+  );
+}
+
+export default App;
